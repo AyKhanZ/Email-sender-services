4b3d665e70e14266648ab61c3b868548f1681580
zeynalovayxan70@gmail.com <zeynalovayxan70@gmail.com>
2026-03-01 22:10:18 +0400

    done

---
+++ EmailSenderApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+var service = new EmailSender();
+await service.SendEmailAsync("zeynalovayxan70@gmail.com","Subject","Hello message6757ytrhgfhjgfjhgjhgjhg");
+
+internal class EmailSender 
+{
+    public async Task SendEmailAsync(string to, string subject, string body)
+    {
+        var smtpServer = "smtp.gmail.com";
+        var userName = "info.atms.org@gmail.com";
+        var password = "gabj gjvf fbwt qcow";
+        var smtpClient = new SmtpClient(smtpServer, 587)
+        {
+            EnableSsl = true,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential(userName, password)
+        };
+        
+        var message = new MailMessage(userName, to, subject, body);
+        await smtpClient.SendMailAsync(message);
+    }
+};
